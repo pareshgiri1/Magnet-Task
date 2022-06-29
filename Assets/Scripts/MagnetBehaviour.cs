874029fee3d6e8ecce88dd58143ec63579b2c6b3
pareshgiri1 <pareshgiri.aparanathi@yudiz.com>
2022-06-29 18:57:11 +0530

    final commit

--- Assets/Scripts/MagnetBehaviour.cs
+++ Assets/Scripts/MagnetBehaviour.cs
@@ -8,55 +8,63 @@ public class MagnetBehaviour : MonoBehaviour
     public Canvas PickUpMessage;
     public GameObject magnetPosition;
     bool magnetWithPlayer;
+    Rigidbody2D magnetRigidBody;
     // Start is called before the first frame update
     void Start()
     {
-       
+        //magnetRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector3 distance = PlayerBehaviour.instance.transform.position - transform.position;
-       if(Vector2.Distance(PlayerBehaviour.instance.transform.position , transform.position) <= 1) 
-        //if(distance.magnitude < 4f)
+        //PickUpMessage.gameObject.SetActive(true);
+        Vector3 distance = PlayerBehaviour.instance.transform.position - transform.position;
+        //if(Vector2.Distance(PlayerBehaviour.instance.transform.position , transform.position) <= 1) 
+        if (distance.magnitude < 3f && magnetWithPlayer == false)
         {
             //Mathf.Abs(distance.magnitude)
-            if(magnetWithPlayer == false)
-            {
-                PickUpMessage.gameObject.SetActive(true);
-            }
-            
-            if (Input.GetKeyDown(KeyCode.E)  )
-            {
-                if (magnetWithPlayer == false)
-                {
-                    MagnetPickup();
-                }
-                else
-                {
-                    ThrowTheMagnet();
-                }
-                    
-            }
-            
+
+            PickUpMessage.gameObject.SetActive(true);
         }
         else
         {
+            //if(magnetWithPlayer == true)
             PickUpMessage.gameObject.SetActive(false);
         }
+
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (magnetWithPlayer == false)
+            {
+                MagnetPickup();
+            }
+            else
+            {
+               
+                ThrowTheMagnet();
+            }
+        }
+
+
+    }   
        
-    }
+    
     void MagnetPickup()
     {
+        Destroy(magnetRigidBody);
         transform.SetParent(magnetPosition.transform);
-        transform.localPosition = new Vector2(0, 1);
+        transform.localPosition = new Vector2(0, 0.5f);
         PickUpMessage.gameObject.SetActive(false);
         magnetWithPlayer = true;
     }
     void ThrowTheMagnet()
     {
         transform.parent = null;
+        magnetRigidBody =  gameObject.AddComponent<Rigidbody2D>()  ;
+        magnetRigidBody.constraints = RigidbodyConstraints2D.FreezeRotation ;
+        magnetRigidBody.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
         PickUpMessage.gameObject.SetActive(true);
         magnetWithPlayer = false;
     }

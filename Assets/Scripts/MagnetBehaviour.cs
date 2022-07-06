49c250f9b6b9081e134f699feadb3a728e779838
pareshgiri1 <pareshgiri.aparanathi@yudiz.com>
2022-07-06 17:53:37 +0530

    final commit

--- Assets/Scripts/MagnetBehaviour.cs
+++ Assets/Scripts/MagnetBehaviour.cs
@@ -21,6 +21,7 @@ public class MagnetBehaviour : MonoBehaviour
     {
         magnetRigidBody = GetComponent<Rigidbody2D>();
         blueMagnet.gameObject.SetActive(false);
+
     }
 
     // Update is called once per frame
